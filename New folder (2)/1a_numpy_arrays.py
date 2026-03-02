17c163aa2e8c55f43832ac3e3bee2a67447b7bf4
myexam0 <alonebeatsz0@gmail.com>
2026-03-02 21:33:55 +0530

    Add files via upload

---
+++ New folder (2)/1a_numpy_arrays.py
@@ -0,0 +1,10 @@
+# Program 1a: Create numpy array, rank1 array, rank2 array and array from tuple
+
+import numpy as np
+
+array = np.array([1, 2, 3])
+rank1_array = np.array([10, 20, 30])
+rank2_array = np.array([[1, 2], [3, 4]])
+tuple_array = np.array((5, 6, 7))
+
+print(array, rank1_array, rank2_array, tuple_array)
